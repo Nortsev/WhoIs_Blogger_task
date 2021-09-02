aee9b6114caa68e9e1d3e1b7ffc8c1301c628969
Vladimir Nortsev <vladimr.nortsev@gmail.com>
2021-09-02 20:43:20 +0400

    Finished version1 task 1

--- Task_1/create_data_base.py
+++ Task_1/create_data_base.py
@@ -6,18 +6,18 @@ sql_create = """ CREATE TABLE IF NOT EXISTS Users(
                         userId INTEGER PRIMARY KEY,
                         age INTEGER NOT NULL );
 
-                        CREATE TABLE IF NOT EXISTS Purchases (
-                        purchaseId INTEGER PRIMARY KEY,
-                        userId INTEGER,
-                        itemId INTEGER,
-                        date  retrieved_time DATE DEFAULT (datetime('now','localtime')),
-                        FOREIGN KEY (userId) REFERENCES Users(userId),
-                        FOREIGN KEY (itemId) REFERENCES Items(itemId));
+                CREATE TABLE IF NOT EXISTS Purchases (
+                purchaseId INTEGER PRIMARY KEY,
+                userId INTEGER,
+                itemId INTEGER,
+                date  retrieved_time DATE DEFAULT (datetime('now','localtime')),
+                FOREIGN KEY (userId) REFERENCES Users(userId),
+                FOREIGN KEY (itemId) REFERENCES Items(itemId));
 
-                        CREATE TABLE IF NOT EXISTS Items(
-                        itemId INTEGER PRIMARY KEY,
-                        price INTEGER
-                        )"""
+                CREATE TABLE IF NOT EXISTS Items(
+                itemId INTEGER PRIMARY KEY,
+                price INTEGER
+                )"""
 
 def create_connection(database=database_file):
     """Create and connection to database"""
@@ -27,36 +27,36 @@ def create_connection(database=database_file):
         cur = conn.cursor()
         cur.executescript(sql_create)
     except Exception as e:
-        print(f'ERROR: {e}')
+        print(f'ERROR create data base: {e}')
     return conn
 
 
-def insert_products(conn,name=database_file):
-    conn = sqlite3.connect(name)
-    cursor = conn.cursor()
-    with open('table_Purchases.json', 'r') as write:
-        datas = json.load(write)
-        sql = """INSERT INTO Purchases(purchaseId,userId,itemId,date ) VALUES(?,?,?,?)"""
-        for data in datas['objects']:
-            table = (data['purchaseId'], data['userId'], data['itemId'], data['date'])
-            print(table)
-            cursor.execute(sql, table)
+def insert_products(conn):
+    """Insert data in data base"""
+    try:
+        cursor = conn.cursor()
+        with open('data/table_purchases.json', 'r') as write:
+            datas = json.load(write)
+            sql = """INSERT INTO Purchases(purchaseId,userId,itemId,date ) VALUES(?,?,?,?)"""
+            for data in datas['objects']:
+                table = (data['purchaseId'], data['userId'], data['itemId'], data['date'])
+                cursor.execute(sql, table)
 
-    with open('table_age.json', 'r') as write:
-        datas = json.load(write)
-        sql = """INSERT INTO Users (userId,age) VALUES(?,?)"""
-        for data in datas['objects']:
-            table = (data['userId'], data['age'])
-            print(table)
-            cursor.execute(sql, table)
+        with open('data/table_users.json', 'r') as write:
+            datas = json.load(write)
+            sql = """INSERT INTO Users (userId,age) VALUES(?,?)"""
+            for data in datas['objects']:
+                table = (data['userId'], data['age'])
+                cursor.execute(sql, table)
 
-    with open('table_item.json', 'r') as write:
-        datas = json.load(write)
-        sql = """INSERT INTO Items (itemId,price) VALUES(?,?)"""
-        for data in datas['objects']:
-            table = (data['itemId'], data['price'])
-            print(table)
-            cursor.execute(sql, table)
+        with open('data/table_item.json', 'r') as write:
+            datas = json.load(write)
+            sql = """INSERT INTO Items (itemId,price) VALUES(?,?)"""
+            for data in datas['objects']:
+                table = (data['itemId'], data['price'])
+                cursor.execute(sql, table)
+    except:
+        print("Таблица уже заполненна ")
 
 
     conn.commit()
--- Task_1/main.py
+++ Task_1/main.py
@@ -1,4 +1,64 @@
 import create_data_base
 
-conn = create_data_base.create_connection()
-create_data_base.insert_products(conn)
+sql_max = """SELECT strftime('%m %Y', date)as date , SUM(Items.price) as summ 
+            FROM Purchases
+            INNER JOIN Users ON Purchases.userId = Users.userId
+            INNER JOIN Items  on Items.itemId = Purchases.itemId 
+            GROUP BY strftime('%YY %m', date) HAVING  Users.age > 35
+            ORDER BY summ DESC 
+            LIMIT 1"""
+
+sql_top_Items = """ SELECT Purchases.itemId,SUM(Items.price) as summ 
+                    FROM Purchases
+                    INNER JOIN Items on Items.itemId = Purchases.itemId
+                    WHERE date > DATETIME('now','-1 YEAR') 
+                    GROUP BY Purchases.itemId 
+                    ORDER BY summ DESC 
+                    LIMIT 1"""
+
+sql_top_3 = """SELECT Items.itemid, SUM(Items.price),
+                SUM(Items.price) * 1.0 / SUM(SUM(Items.price)) OVER () as Fraction
+                FROM Purchases 
+                INNER JOIN Items
+                ON Items.itemid = Purchases.itemId
+                WHERE Purchases.date >= '2020-01-01' AND
+                      Purchases.date < '2021-01-01'
+                GROUP BY Items.itemid
+                ORDER BY Fraction DESC
+                LIMIT 3;"""
+
+
+def summ(min, max):
+    sql = (f""" SELECT AVG(price)
+               FROM Items 
+               INNER JOIN Purchases ON Items.itemId = Purchases.itemId
+               INNER JOIN Users ON Purchases.userId = Users.userId 
+               where Users.age BETWEEN {min} AND {max};""")
+    return sql
+
+
+def main():
+    conn = create_data_base.create_connection()
+    create_data_base.insert_products(conn)
+    cursor = conn.cursor()
+    i = 1
+    result = cursor.execute(summ(18, 25))
+    print(
+        f'Средння сумма трат пользователей в возрастном диапазоне от 18 до 25 лет включительно = {int(result.fetchone()[0])}p')
+    result = cursor.execute(summ(26, 35))
+    print(
+        f'Средння сумма трат пользователей в возрастном диапазоне от 26 до 35 лет включительно = {int(result.fetchone()[0])}p')
+    result = cursor.execute(sql_max)
+    print(f'Самая большая выручка от пользователей в возрастном диапазоне 35+  = {result.fetchone()[0]}')
+    result = cursor.execute((sql_top_Items))
+    print(f'Саммую большую выручку за последний год дает товар с Id = {result.fetchone()[0]}')
+    result = cursor.execute(sql_top_3)
+    print("Топ 3 товара: ")
+    for results in result:
+        print(
+            f"Товар №{i} c id {results[0]} c выручкой {results[1]}. Его доля в общем количестве продаж {round(results[2], 2)}")
+        i += 1
+
+
+if __name__ == '__main__':
+    main()
